bcd61ad3bffc188768e6199b4c7071340c21d87a
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2024-11-02 07:26:36 +0530

    Name change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Thinira.</ThemedText>
+        <ThemedText type="title">Welcome Thinira Nilushan.</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
